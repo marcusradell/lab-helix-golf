d2a0af82c49e5a6d48d31070a4441b66b8d13f11
Marcus Rådell <marcus@radell.net>
2025-05-22 12:01:46 +0200

    setup project and solve first golf

---
+++ src/object-into-array.ts
@@ -0,0 +1,12 @@
+const palette = {
+  apricot: "#f47868",
+  lightning: "#ffcd1c",
+  delta: "6f44f0",
+};
+
+// Solution
+// const palette = [
+//   ["apricot", "#f47868"],
+//   ["lightning", "#ffcd1c"],
+//   ["delta", "6f44f0"],
+// ];
